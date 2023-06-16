e8d480bbbe6db9e5b3acdf33171c1a19604ff29e
Mohammed-152001 <mame66091@gmail.com>
2023-06-17 00:45:49 +0300

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Prints the alphabet.
  *
@@ -8,6 +9,7 @@ int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
+
 	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
